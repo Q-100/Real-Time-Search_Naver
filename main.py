c046c81f1c4da9d4dbfac84fb7de8c56918e6f6b
Q-100 <opea5954@gmail.com>
2021-03-02 06:47:44 +0900

    Test

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import urllib.request
+from bs4 import BeautifulSoup
+from datetime import date
+from collections import Counter
 
+hds = {"User-Agent": "Mozilla/5.0"}
+url = "https://news.naver.com/main/list.nhn?mode=LSD&mid=sec&sid1=001&listType=title&date=20210302"
+
+baseUrl = urllib.request.Request(url,headers=hds)
+# date = "".join(date.isoformat(date.today()).split("-"))
+# url = baseUrl + urllib.parse.quote_plus(date)
+html = urllib.request.urlopen(baseUrl)
+
+soup = BeautifulSoup(html, "html.parser")
+tmp = []
+for i in soup.find_all(class_="nclicks(fls.list)"):
+    tmp += i.text.split(" ")
+
+tmp1 = Counter(tmp).most_common(5)
+print(tmp1)
